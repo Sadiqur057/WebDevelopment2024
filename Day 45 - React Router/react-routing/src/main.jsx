27552340aeff8024f3c45dafed4f08972d842624
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-20 15:05:05 +0600

    used react router

--- Day 45 - React Router/react-routing/src/main.jsx
+++ Day 45 - React Router/react-routing/src/main.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello from Home Page</div>
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ]
   },
-  {
-    path: "/about",
-    element: <div>Hello from About Page</div>
-  }
-])
+  
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
